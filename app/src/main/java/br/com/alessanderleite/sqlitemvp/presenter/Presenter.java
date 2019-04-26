44ec4e054c34bdd9be472ebbe18af9137a5e43c2
alessanderleite <alessander.logistica@gmail.com>
2019-04-25 22:09:41 -0300

    code the Presenter.java class

--- app/src/main/java/br/com/alessanderleite/sqlitemvp/presenter/Presenter.java
+++ app/src/main/java/br/com/alessanderleite/sqlitemvp/presenter/Presenter.java
@@ -1,4 +1,24 @@
 package br.com.alessanderleite.sqlitemvp.presenter;
 
-public class Presenter {
+import java.util.List;
+
+import br.com.alessanderleite.sqlitemvp.model.IModel;
+import br.com.alessanderleite.sqlitemvp.model.Model;
+import br.com.alessanderleite.sqlitemvp.view.IMainActivity;
+
+public class Presenter implements IPresenter {
+
+    private IMainActivity iMainActivity;
+    private IModel iModel;
+
+    public Presenter(IMainActivity iMainActivity) {
+        this.iMainActivity = iMainActivity;
+        iModel = new Model(iMainActivity);
+    }
+
+    @Override
+    public void setDataToListView() {
+        List<String> list = iModel.getListFromDatabase();
+        iMainActivity.setDataToListview(list);
+    }
 }
