0145974c1d2d2e3b5c2d2789f84bef93abdaa2b2
alessanderleite <alessander.logistica@gmail.com>
2019-04-25 22:19:26 -0300

    code the Model.java class

--- app/src/main/java/br/com/alessanderleite/sqlitemvp/model/Model.java
+++ app/src/main/java/br/com/alessanderleite/sqlitemvp/model/Model.java
@@ -1,4 +1,36 @@
 package br.com.alessanderleite.sqlitemvp.model;
 
-public class Model {
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.alessanderleite.sqlitemvp.view.DataBaseOpenHelper;
+import br.com.alessanderleite.sqlitemvp.view.IMainActivity;
+
+public class Model implements IModel {
+
+    private SQLiteDatabase database;
+
+    public Model(IMainActivity iMainActivity) {
+        database = new DataBaseOpenHelper((Context) iMainActivity).getWritableDatabase();
+    }
+
+    @Override
+    public List<String> getListFromDatabase() {
+
+        List<String> list = new ArrayList<>();
+        String sqlQueryText = "SELECT adam, til from test";
+        Cursor cursor = this.database.rawQuery(sqlQueryText, null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            list.add(cursor.getString(0));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        database.close();
+        return list;
+    }
 }
