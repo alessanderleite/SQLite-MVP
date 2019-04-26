1b55c249834abeb4f6ae5c723448dce0d44c1e86
alessanderleite <alessander.logistica@gmail.com>
2019-04-25 21:40:19 -0300

    code the DataBaseOpenHeldper

--- app/src/main/java/br/com/alessanderleite/sqlitemvp/view/DataBaseOpenHelper.java
+++ app/src/main/java/br/com/alessanderleite/sqlitemvp/view/DataBaseOpenHelper.java
@@ -1,4 +1,15 @@
 package br.com.alessanderleite.sqlitemvp.view;
 
-public class DataBaseOpenHelper {
+import android.content.Context;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+public class DataBaseOpenHelper extends SQLiteAssetHelper {
+
+    private static final String DATABASE_NAME = "test.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DataBaseOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 }
